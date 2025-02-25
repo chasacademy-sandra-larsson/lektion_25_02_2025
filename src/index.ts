2c2bd7ff9b4ef3d2ea3c5e2ad5bcc01246c98384
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-02-25 11:50:27 +0100

    added ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //const express = require("express") // common js
-import express from "express";
+import express, {Request, Response } from "express";
 import cors from "cors"
 //import bodyParser from "body-parser" // Gamla sättet
 
@@ -13,25 +13,24 @@ app.use(express.json()); // Den hör gör så att man kan hantera inkommande JSO
 //app.use(bodyParser())// Gamla sättet 
 
 // Routes - endpoint - behöver URL och HTTP-metod för att triggas
-app.get("/whatever", (request, response) => {
+app.get("/whatever", (request: Request, response: Response) => {
     response.send("Hej från rutt / med metod GET")
 });
 
-
 // Som att skicka data med ett formulär!
-app.post("/whatever", (req, res) => {
+app.post("/whatever", (req: Request, res: Response) => {
     const {firstname, lastname} = req.body;
     res.json({ message: `Hello from /whatever with a POST-request and ${firstname} ${lastname}`})
 });
 
-
 // Att uppdatera en record 
-app.put("/whatever", (req, res) => {
+app.put("/whatever", (req: Request, res: Response) => {
     const { lastname } = req.body
     res.json({ message: `Hello from /whatever with a PUT-request and updated ${lastname}`})
 })
 
-app.delete("/whatever", (req, res) => {
+// En förfrågan om delete till servern
+app.delete("/whatever", (req: Request, res: Response) => {
     res.json({message: "A delete request was made to this server"})
 })
 
